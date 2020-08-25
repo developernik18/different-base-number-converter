f9b1fa2608f432bc55e62d8d5fd90ec2095f6490
Nikhil Kumar <developernik18@gmail.com>
2020-08-25 12:54:50 +0530

    Basic Layout of converter
    Basic Input field and 4 output field layout created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      inputValue:0,
+      decimalValue:0,
+      binaryValue:0,
+      octalValue:0,
+      hexadecimalValue:0
+    };
+  }
+
+  render() {
+    return (
+      <div className="center">
+        <h1> Number Converter </h1>
+        <input type="number"
+         placeholder="Enter a Decimal Number"
+         value={this.state.inputValue} 
+        /> 
+        <table class="center mobile-view margin-space">
+          <caption><h2> Converted values </h2></caption>
+          <tr>
+            <th>
+              Decimal Value
+            </th>
+            <th>
+              Binary Value
+            </th>
+            <th>
+              Octal Value
+            </th>
+            <th>
+              Hexadecimal Value
+            </th>
+          </tr>
+          <tr>
+            <td>
+              { this.state.decimalValue }
+            </td>
+            <td>
+              { this.state.binaryValue }
+            </td>
+            <td>
+              { this.state.octalValue }
+            </td>
+            <td>
+              { this.state.hexadecimalValue }
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
