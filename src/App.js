8755b883066574c1f39e266771336c0fba5aa367
Nikhil Kumar <developernik18@gmail.com>
2020-08-25 17:27:26 +0530

    function added for decimal to other base conversion
    Added function for conversion of decimal number to binary number, octal number and hexadecimal number and output them to display

--- src/App.js
+++ src/App.js
@@ -20,6 +20,58 @@ class App extends React.Component {
 
   onInputChangeHandler(ev) {
     this.setState({inputValue: ev.target.value});
+    const binValue = this.calculateBinaryValue(ev.target.value);
+    const octValue = this.calculateOctalValue(ev.target.value);
+    const hexValue = this.calculateHexadecimalValue(ev.target.value);
+
+    let outputValue = {
+      decimalValue: ev.target.value,
+      binaryValue : binValue,
+      octalValue: octValue,
+      hexadecimalValue: hexValue
+    }
+
+    this.setState({outputValue: outputValue})
+    
+  }
+
+  calculateBinaryValue(input) {
+    let binStr = ''
+    
+    while(input){
+      let r = input%2;
+      input = Math.floor(input/2);
+      binStr = r + binStr;
+    }
+
+    return binStr;
+  }
+
+  calculateOctalValue(input) {
+    let octStr = ''
+    
+    while(input){
+      let r = input%8;
+      input = Math.floor(input/8);
+      octStr = r + octStr;
+    }
+
+    return octStr;
+  }
+
+  calculateHexadecimalValue(input) {
+    let hexStr = ''
+    
+    while(input){
+      let r = input%16;
+      input = Math.floor(input/16);
+      if(r>9){
+        r = String.fromCharCode(r+55);
+      }
+      hexStr = r + hexStr;
+    }
+
+    return hexStr;
   }
 
   render() {
